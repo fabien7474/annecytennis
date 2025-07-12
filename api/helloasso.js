91965c2a2474211b4377ef1c1a242ce82c4ae1e1
Fabien <fabien7474@gmail.com>
2025-07-12 22:37:43 +0200

    Update helloasso.js

--- api/helloasso.js
+++ api/helloasso.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
     const payload = req.body;
-    console.log("Notification reçue de HelloAsso :", payload);
+    const payoadJson = JSON.stringify(payload, null, 2);
+    console.log("Notification reçue de HelloAsso :", payoadJson);
   
     // Ex: envoyer un email, enregistrer dans un Google Sheet, etc.
   
